2993aab1104baf5ec7ea8d9cb9d9b30f5dd66bc5
nakashun-t <nakashun.t@gmail.com>
2021-02-20 17:07:50 +0900

    first commit

---
+++ Scripts/CustomPackageTestScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Nakashun
+{
+    public class CustomPackageTestScript : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hello from Custom Package script");
+        }
+    }
+}
